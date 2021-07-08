6662be6451a6e192591b19bc727a7f0b860cbc6c
Cyprien OTTO <cyprienotto@outlook.fr>
2021-07-09 01:55:07 +0200

    2 OOP (Getters and Setters)

--- 2 OOP/src/com/ControlFlow/Main.java
+++ 2 OOP/src/com/ControlFlow/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         var employee = new Employee();
-        employee.baseSalary =  50_000;
-        employee.extrHours =  10;
-        employee.hourlyRate =  20;
+        employee.setBaseSalary(50_000);
+        employee.setExtrHours(10);
+        employee.setHourlyRate(20);
 
         int wage = employee.calculateWage();
 
