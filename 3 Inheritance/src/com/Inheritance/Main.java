4685d93f55f0464d2eb7ec769a21bb7efceb73a0
Cyprien OTTO <cyprienotto@outlook.fr>
2021-07-10 19:42:57 +0200

    4 Inheritance ( Upcasting Downcasting )

--- 3 Inheritance/src/com/Inheritance/Main.java
+++ 3 Inheritance/src/com/Inheritance/Main.java
@@ -7,4 +7,12 @@ public class Main {
     public static void main(String[] args) {
         // var uicontrol = new UIControl();
     }
+
+    public static void show(UIControl control) {
+        if(control instanceof TextBox) {
+            var textBox = (TextBox) control;
+            textBox.setText("Hello Upcasting Downcasting");
+        }
+        System.out.println(control);
+    }
 }
