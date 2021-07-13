a6570a58e5e49721713efa8b5f5d342a1d6fd9ab
cyprienotto <56775432+cyprienotto@users.noreply.github.com>
2021-07-13 20:45:28 +0200

    Update GenericsList.java

--- 5 Exceptions - generics - collections - lambda expressions & functions - streams - concurrency & multi-threading - executive framework/src/com/cyprienotto/generics/GenericsList.java
+++ 5 Exceptions - generics - collections - lambda expressions & functions - streams - concurrency & multi-threading - executive framework/src/com/cyprienotto/generics/GenericsList.java
@@ -3,7 +3,7 @@ package com.cyprienotto.generics;
 // "T" : convention  letter for "type", only Reference type
 // for store primitive type  use wrapper class (Integer, Float, Boolean etc..).
 
-public class GenericsList<T extends Comparable & Cloneable> { // Only object which could  be comparable & cloneable
+public class GenericsList<T extends Comparable & Cloneable> { // Only store object that could  be comparable & cloneable
     private T[] items = (T[]) new Object[10];
     private int count;
 
