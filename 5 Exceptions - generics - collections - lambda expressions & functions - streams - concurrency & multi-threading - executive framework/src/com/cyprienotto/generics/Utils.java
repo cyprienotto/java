ccad80b8cbf6f98b77bf8df8ba687d8f0e13d979
Cyprien OTTO <cyprienotto@outlook.fr>
2021-07-14 01:41:45 +0200

    Generic Methods

---
+++ 5 Exceptions - generics - collections - lambda expressions & functions - streams - concurrency & multi-threading - executive framework/src/com/cyprienotto/generics/Utils.java
@@ -0,0 +1,9 @@
+package com.cyprienotto.generics;
+
+public class Utils {
+    // static so that we do not need to create an instance of  the class.
+    // <T> before the return type  which is T here
+    public static <T extends Comparable<T>> T max( T first, T second) {
+        return ( first.compareTo(second) < 0 ) ? second : first;
+    }
+}
